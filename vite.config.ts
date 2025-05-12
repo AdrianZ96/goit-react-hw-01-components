e7c8fe28ca213c58a1f70cd096d6a27e679df257
Adrian Zdziarski <adrianzdziarski96@gmail.com>
2025-05-12 20:28:49 +0200

    Fix build output: index.html to docs root

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/goit-react-hw-01-components/',
   plugins: [react()],
+  build: {
+    outDir: 'docs',      // Wszystko wyląduje w folderze `docs`
+    assetsDir: 'assets', // Assety w `docs/assets/`
+    emptyOutDir: true    // Na czysto przed każdym buildem
+  }
 });
